cd7dc9827661b4b1b5bf526dae859ed582e0ae03
Saman <saman.khataei@hotmail.com>
2023-05-21 19:27:28 +0300

    Chat development progress
    Users are added to the main page
    empty chats are added to the firestore based on the combined ids
    improvements

--- chat-now-app/src/Components/SideBar/SideBar.jsx
+++ chat-now-app/src/Components/SideBar/SideBar.jsx
@@ -1,42 +1,68 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import Navbar from "../Navbar/Navbar";
 import Searchbar from "../Searchbar/Searchbar";
 import { Avatar, CardHeader, IconButton } from "@mui/material";
 import img from "../../assets/image/profile-photo.jpg";
 import Card from "@mui/material/Card";
+import { Context } from "../../Context/AuthContext";
+import { useEffect } from "react";
+import { createChatIfNotExistOrGetMessages } from "../../Firebase";
+import { ChatContext } from "../../Context/ChatContext";
 
-const users = ["John", "Jane", "Niki"];
 const SideBar = () => {
-  const [user, setUser] = useState(users);
+  const { users, currentUser } = useContext(Context);
+  const { chats } = useContext(ChatContext);
+  const [people, setPeople] = useState([]);
+  useEffect(() => {
+    setPeople(users);
+  }, [users, chats]);
+  const handleSelectUser = async (selectedUser) => {
+    const massagesObject = await createChatIfNotExistOrGetMessages(
+      currentUser.uid,
+      selectedUser.uid
+    );
+    selectedUser.messages = massagesObject.messages;
+  };
 
   return (
     <>
       <Navbar></Navbar>
-      <Searchbar users={users}></Searchbar>
+      <Searchbar users={users} setPeople={setPeople}></Searchbar>
 
-      {users.map((user) => {
+      {people.map((user) => {
         return (
-          <Card
-            key={user}
-            elevation={0}
-            sx={{
-              bgcolor: "inherit",
-              opacity: "0.8",
-              cursor: "pointer",
-              "&:hover": {
-                bgcolor: "#696969",
-              },
-            }}
-          >
-            <CardHeader
-              avatar={<Avatar sx={{ bgcolor: "inherit" }} src={img}></Avatar>}
-              action={<IconButton aria-label="settings"></IconButton>}
-              title={user}
-              subheader="hello John"
-              subheaderTypographyProps={{ color: "#FFF8DC", fontSize: "12px" }}
-              sx={{ color: "#FFF8DC" }}
-            />
-          </Card>
+          currentUser.uid !== user.uid && (
+            <Card
+              key={user}
+              elevation={0}
+              sx={{
+                bgcolor: "inherit",
+                opacity: "0.8",
+                cursor: "pointer",
+                "&:hover": {
+                  bgcolor: "#696969",
+                },
+              }}
+              onClick={() => handleSelectUser(user)}
+            >
+              <CardHeader
+                avatar={
+                  <Avatar
+                    sx={{ bgcolor: "inherit" }}
+                    src={user.photoURL}
+                  ></Avatar>
+                }
+                action={<IconButton aria-label="settings"></IconButton>}
+                title={user.name}
+                subheader={user.messages[user.messages.length - 1]}
+                subheaderTypographyProps={{
+                  color: "#FFF8DC",
+                  fontSize: "12px",
+                }}
+                sx={{ color: "#FFF8DC" }}
+              />
+            </Card>
+          )
         );
       })}
     </>
